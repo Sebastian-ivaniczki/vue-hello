23f71f92974aaca72f64124e9df0b5ba5323d0c7
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-01-09 14:27:02 +0100

    added img

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
   createApp({
     data() {
       return {
-        message: 'Vue Hello'
+        message: 'Vue Hello',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'
       }
     }
   }).mount('#root')
